f5abe53540101eb3cfa76651c275a6d22df93ed6
Barbara Fick <bkantzos@yahoo.com>
2019-01-10 14:24:33 -0700

    working login with google+ api

---
+++ config/passport-setup.js
@@ -0,0 +1,69 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
+
+// Requiring our Todo model
+var db = require("../models");
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+
+    db.User.findOne({
+        where: {
+            id: id
+        }
+    }).then(function (data) {
+        done(null, id);
+    });
+});
+
+passport.use(
+    new GoogleStrategy({
+        // options for google strategy
+        callbackURL: '/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+        //accessToken receive from google that if we want to alter the users profile
+        //   allows us to go to into their mailbox, read their emails
+        //refreshToken - refresh the accessToken which expires after a certain amount of time
+        //profile - takes code returned from google and send that back to google to get the profile info
+        //function called done
+    }, (accessToken, refreshToken, profile, done) => {
+        // passport callback function
+        console.log("passport callback function fired");
+        console.log(`retrieved google profile ${profile}`);
+        //now take some of this data and save that in our database
+        //interacting wtih the db via the User model
+
+        //check if user already exists in our database
+        db.User.findOne({
+            where: {
+                google_id: profile.googleId
+            }
+        }).then(function (data) {
+            console.log(`hit db for user data ${data}`);
+            //is this the way to check for 0/1 record?
+            if (!data) {
+                db.User.create({
+                    user_name: profile.displayName,
+                    google_id: profile.googleId,
+                    thumbnail: profile._json.image.url
+                }).then(function (data) {
+                    console.log(`New User created:  ${data}`);
+                    done(null, data);
+                });
+            } else {
+                console.log(`User ${data.user_name} already exists`);
+                done(null, data);
+            }
+
+        });
+
+
+
+    })
+);
+
