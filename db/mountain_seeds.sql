856580b572b9e9b7bf3790e4766833ebf91c9b88
rkoven74 <roxannekoven@gmail.com>
2019-01-10 12:28:20 -0700

    created mountain and route tables

---
+++ db/mountain_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO Mountains (rank, peakName, elevation, latitude, longitude, mountainRange) VALUES (1, "Mount Elbert", 14433, 39.117816, -106.4452306, "Sawatch Range");
+INSERT INTO Mountains (rank, peakName, elevation, latitude, longitude, mountainRange) VALUES (2, "Mount Massive", 14421, 39.187298, -106.3206180, "Sawatch Range");
+INSERT INTO Mountains (rank, peakName, elevation, latitude, longitude, mountainRange) VALUES (5, "La Plata Peak", 14336, 39.029251, -106.4731450, "Sawatch Range");
+
