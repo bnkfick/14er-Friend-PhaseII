15df25261f32a9b3c14b790ca36273ac3b5086a1
AChapman <achapman77@gmail.com>
2019-01-11 16:20:39 -0700

    Cleaned up climbing preferences box and added toggle

---
+++ public/assets/js/preference-selector.js
@@ -0,0 +1,33 @@
+$(function () {
+//Everything inside this function! :)
+    
+//Toggle #about partial
+    //Delete .hide() after dev
+    $(`#about`).hide();
+
+    $(`#about`).on(`click`, function () {
+        $(`#about`).slideUp("slow");
+    });
+    
+//Toggle #preference-box partial
+    //Show/Hide depending on Login Status
+    $(`#preference-box`).hide()
+    
+    $(`#preferences`).on('click', `#toggle-preference-box`, function () {
+        var btn = $(`#preference-btn` )
+
+        if (btn.hasClass(`fa-plus-square`)) {
+            btn.removeClass(`fa-plus-square`)
+        } else {
+            btn.addClass(`fa-plus-square`);
+            // $(this).parent().parent().parent().addClass(`ptb-25`)
+        };
+        $(`#preference-box`).slideToggle(500, `swing`, function () {
+            
+        });
+    })
+
+    
+
+
+});
