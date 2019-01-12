e2e6983f2d208d7329c354cc04ac594d6ef19128
Barbara Fick <bkantzos@yahoo.com>
2019-01-11 21:44:52 -0700

    working on sequelize statement joining mountain and mountain route tables

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
   //use res.render to load up an ejs view file
@@ -20,11 +20,39 @@ module.exports = function(app) {
   app.get('/', function (req, res) {
     //get mtn info with sequelize 
     db.Mountain.findAll({})
-    .then(function(data) {
-      //loop over mountains and get all routes for each mountain
-      console.log(data);
-      res.render('pages/index', { mtns: data} );
-    });
+      .then(function (data) {
+        var MtnsWRoutes;
+        if (data) {
+          //loop over mountains and get all routes for each mountain
+          for (var x = 0; x < data.length; x++) {
+            //console.log(data);
+            
+            //console.log(data[x].id);
+
+            db.Mountain.findAll({
+              include: {
+                model: db.Mountain_route,
+                where: {
+                  MountainId: data[x].id
+                },
+                required: false
+              }
+            }).then(function (routes_data) {
+              //console.log(routes_data);
+
+              for (var y = 0; y < routes_data; y++ ) {
+                MtnsWRoutes.push(routes_data[y]);
+              }
+              //put the routes on the data ojbect here 
+              //put the route data on the object to send
+            })
+
+          }
+          console.log(MtnsWRoutes);
+          res.render('pages/index', { mtns: MtnsWRoutes });
+        }
+
+      });
   });
 
 
@@ -33,27 +61,27 @@ module.exports = function(app) {
   // app.get("/", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/index.html"));
   // });
-    
-    // mountain route loads expanded view of single mountain and all it's routes + pictures, route conditions chat, etc
-//   app.get("/mountains/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/mountain.html"));
-//   });
-    
-    // admin route loads page  with sections for managing users, form for entering mtn info, route info, monitoring comments, and user route updates/images
-//   app.get("/admin/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/admin.html"));
-//   });
 
-    // user-settings route loads account preferences page: upload user image, create groups, add friends, etc
-//   app.get("/user-settings/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/user-settings.html"));
-//   });
-    
-        // team route loads team dev page!
-//   app.get("/team/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/user-settings.html"));
-//   });
-    
-  
+  // mountain route loads expanded view of single mountain and all it's routes + pictures, route conditions chat, etc
+  //   app.get("/mountains/", function(req, res) {
+  //     res.sendFile(path.join(__dirname, "../public/mountain.html"));
+  //   });
+
+  // admin route loads page  with sections for managing users, form for entering mtn info, route info, monitoring comments, and user route updates/images
+  //   app.get("/admin/", function(req, res) {
+  //     res.sendFile(path.join(__dirname, "../public/admin.html"));
+  //   });
+
+  // user-settings route loads account preferences page: upload user image, create groups, add friends, etc
+  //   app.get("/user-settings/", function(req, res) {
+  //     res.sendFile(path.join(__dirname, "../public/user-settings.html"));
+  //   });
+
+  // team route loads team dev page!
+  //   app.get("/team/", function(req, res) {
+  //     res.sendFile(path.join(__dirname, "../public/user-settings.html"));
+  //   });
+
+
 
 };
