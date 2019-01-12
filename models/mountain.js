eb61a9cd4ef35178b4661e81013516e486ab41b2
Barbara Fick <bkantzos@yahoo.com>
2019-01-12 12:52:32 -0700

    working on getting routes, but data losing scope in callbacks

--- models/mountain.js
+++ models/mountain.js
@@ -41,6 +41,9 @@ module.exports = function(sequelize, DataTypes) {
         validate: {
           len: [1,140]
         }
+      },
+      weatherLink: {
+        type: DataTypes.STRING,
       }
     });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,42 +19,103 @@ module.exports = function (app) {
   //index page
   app.get('/', function (req, res) {
     //get mtn info with sequelize 
-    db.Mountain.findAll({})
+    var MtnsArr = [];
+    db.Mountain.findAll({
+      // Add order conditions here....
+      order: [
+        ['rank', 'ASC'],
+      ],
+    })
       .then(function (data) {
-        var MtnsWRoutes;
+
         if (data) {
+          console.log("THERE ARE " + data.length + " RESULTS." );
           //loop over mountains and get all routes for each mountain
           for (var x = 0; x < data.length; x++) {
-            //console.log(data);
-            
-            //console.log(data[x].id);
 
-            db.Mountain.findAll({
-              include: {
-                model: db.Mountain_route,
+            // console.log("id: " + data[x].id);
+            // console.log("rank: " + data[x].rank);
+            // console.log("peakName: " + data[x].peakName);
+            // console.log("elevation: " + data[x].elevation);
+
+            // var out = '';
+            // for (var p in data) {
+            //   out += p + ': ' + data[p] + '\n';
+            // }
+            //console.log(out);
+            //Get the mountain routes for the current mountain
+            db.Mountain_route.findAll({
                 where: {
                   MountainId: data[x].id
-                },
-                required: false
-              }
+                }
             }).then(function (routes_data) {
-              //console.log(routes_data);
-
+  
+              var mtnObj = {
+                mtnData: data[x],
+                trails: []
+              };
+              console.log("================");
+              console.log(mtnObj);
               for (var y = 0; y < routes_data; y++ ) {
-                MtnsWRoutes.push(routes_data[y]);
+                  //put the routes on the data ojbect here 
+                  mtnObj.trails.push(routes_data[y]);
               }
-              //put the routes on the data ojbect here 
               //put the route data on the object to send
-            })
 
+              MtnsArr.push(mtnObj);
+              console.log(MtnsArr);
+            });
+            
           }
-          console.log(MtnsWRoutes);
-          res.render('pages/index', { mtns: MtnsWRoutes });
+          console.log(MtnsArr.length);
+          res.render('pages/index', { mtns: MtnsArr });
         }
 
       });
   });
 
+
+  /*   app.get('/', function (req, res) {
+      //get mtn info with sequelize 
+      db.Mountain.findAll({
+        // Add order conditions here....
+        order: [
+            ['rank', 'ASC'],
+        ],})
+        .then(function (data) {
+          var Mtns;
+          if (data) {
+            //loop over mountains and get all routes for each mountain
+            for (var x = 0; x < data.length; x++) {
+              //console.log(data);
+              
+              //console.log(data[x].id);
+  
+              db.Mountain.findAll({
+                include: {
+                  model: db.Mountain_route,
+                  where: {
+                    MountainId: data[x].id
+                  },
+                  required: false
+                }
+              }).then(function (routes_data) {
+                //console.log(routes_data);
+  
+                for (var y = 0; y < routes_data; y++ ) {
+                  MtnsWRoutes.push(routes_data[y]);
+                }
+                //put the routes on the data ojbect here 
+                //put the route data on the object to send
+              })
+  
+            }
+            console.log(MtnsWRoutes);
+            res.render('pages/index', { mtns: MtnsWRoutes });
+          }
+  
+        });
+    }); */
 
   //Not needed with EJS route above
   // index route loads everything: user-box, mountains-table, routes-table, route-beta-table
