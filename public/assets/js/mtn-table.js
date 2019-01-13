eb3c3b051322ba7f65819fadd24b7d8c22225c8f
AChapman <achapman77@gmail.com>
2019-01-12 23:57:33 -0700

    Toggle for Route beta working

--- public/assets/js/mtn-table.js
+++ public/assets/js/mtn-table.js
@@ -4,7 +4,7 @@ $(function () {
     //Toggle Routes View----------------------------------------
     $(".routes-table").hide();
     
-    $("#table-list").on("click", ".toggle-show-routes-btn", function () {
+    $(".conditions").on("click", ".toggle-show-routes-btn", function () {
         if ($(this).find(`i`).hasClass("fa-plus-square")) {
             $(this).find(`i`).removeClass("fa-plus-square")
         } else {
@@ -18,7 +18,21 @@ $(function () {
         });
     });
     
+    //Toggle Route Beta View
+    $(".route-beta").hide();
+
+    $(".route-info").on("click", ".toggle-show-route-beta-btn", function () {
+        if ($(this).find(`i`).hasClass("fa-map-marked-alt")) {
+            $(this).find(`i`).removeClass("fa-map-marked-alt")
+        } else {
+            $(this).find(`i`).addClass("fa-map-marked-alt");
+        }
+        var routeID = $(this).find(`i`).attr("id");
+        console.log(routeID)
         
+        $(`#${routeID}-beta`).slideToggle(500, "swing", function () {  
+        });
+    });
     
 });
     
