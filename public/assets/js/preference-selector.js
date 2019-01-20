d57a4dd0477530af4046dc59b26a0c530179e0be
Barbara Fick <bkantzos@yahoo.com>
2019-01-19 20:04:31 -0700

    dynamic js loading based on url.  test for null data in preference-loader.js

--- public/assets/js/preference-selector.js
+++ public/assets/js/preference-selector.js
@@ -335,30 +335,3 @@ $(function () {
         );
     });
 });
-
-
-
-$("document").ready(function(){
-
-    var userid = $("#preferences-submit-btn").data("userid");
-    //console.log(userid);
-
-    $.get("/api/user/preferences/" + userid, function (data) {
-        //console.log(data);
-        
-        $("#min-temp-slider").slider('setValue', data.tempMin, true);
-        $("#max-wind-slider").slider('setValue', data.windLimit, true);
-        $("#max-precip-slider").slider('setValue', data.precipLimit, true);
-        $("#max-dist-slider").slider('setValue', data.distMax, true);
-
-    });
-    $.get("/api/user/profile/" + userid, function (data) {
-        //console.log(`profile ${data}`);
-        if (data) {
-            $("#fname-input").val(data.firstname);
-            $("#lname-input").val(data.lastname);
-            $("#mobile-input").val(data.mobile);
-            $("#bio-area").val(data.bio);
-        }
-    });
-});
